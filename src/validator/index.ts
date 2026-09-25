060d7d94dd151c598500c3ee9123c846bde4ba0d
ghostroller <ghostashadow@outlook.com>
2026-09-26 04:45:27 +0800

    feat(validator): verify fixed materials against durable source content

---
+++ src/validator/index.ts
@@ -0,0 +1,4 @@
+export { ValidatorService, ValidatorError } from './service';
+export { CapturedJsonSourceReader } from './captured-json-source';
+export type { CapturedSourceLocation } from './captured-json-source';
+export type * from './types';
